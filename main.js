678774cb76b4c70144bdfb9b8dcdf92d6d9991f4
Karla Benitez <karlamaths@gmail.com>
2025-10-15 11:38:08 -0600

    Se agrega la función resta en el main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-functionsuma(num1, num2){
+function suma(num1, num2){
 	let resultado = num1 + num2;
 	return resultado;
 }//suma
+
+function resta(num1, num2){
+	let resultado = num1 - num2;
+	return resultado;
+}//resta
